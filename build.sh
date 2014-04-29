d21714374405b6cb4b7ce1832603b66fd882e789
monai <monai@cure.lt>
2014-04-29 21:40:56 +0300

    added support for saucy64

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ set -e
 
 #export PACKER_LOG=1
 
-NAME='saucy64'
+NAME='trusty64'
 TYPE="virtualbox-iso"
 
 rm -f packer_${TYPE}_virtualbox.box
 rm -rf output-${TYPE}
-packer build -only $TYPE packer.json
+packer build -only $TYPE trusty64.json
 mv packer_${TYPE}_virtualbox.box ${NAME}.box
 vagrant box remove $NAME || true
 vagrant box add $NAME ${NAME}.box
