57df1a0c69dbcb8b0992743b1430c3c4e9cf8aa2
monai <monai@cure.lt>
2014-10-02 19:12:18 +0300

    dont' install puppet

--- provision/root.sh
+++ provision/root.sh
@@ -3,12 +3,12 @@
 set -e
 
 # Update and upgrade dependencies
-apt-get update -y -qq > /dev/null
-apt-get upgrade -y -qq > /dev/null
-apt-get autoremove -y -qq > /dev/null
+apt-get update -y -qq >/dev/null
+apt-get upgrade -y -qq >/dev/null
+apt-get autoremove -y -qq >/dev/null
 
 # Install necessary libraries for guest additions and Vagrant NFS Share
-apt-get -y -qq install build-essential dkms nfs-common > /dev/null
+apt-get -y -qq install build-essential dkms nfs-common >/dev/null
 
 # NOPASSWD sudo for group "admin"
 groupadd -r admin
@@ -17,15 +17,12 @@ cp /etc/sudoers /etc/sudoers.back
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 
-# Install Puppet
-apt-get -y -qq install puppet > /dev/null
-
 # Install Guest Additions
 VERSION="$(cat .vbox_version)"
 MPOINT="/mnt/cdrom"
 mkdir -p $MPOINT
 mount -o loop VBoxGuestAdditions_${VERSION}.iso $MPOINT
 # Suppress "Could not find the X.Org or XFree86 Window System, skipping."
-sh ${MPOINT}/VBoxLinuxAdditions.run 2>&1 > /dev/null || true
+sh ${MPOINT}/VBoxLinuxAdditions.run 2>&1 >/dev/null || true
 umount $MPOINT
 rm -rf $MPOINT
